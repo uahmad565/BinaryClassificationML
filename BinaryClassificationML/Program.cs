dbb1f85d07f5b4a7e4cddf64ea4254b03c14f09e
Usman Kabir <usman.kabir@netwrix.com>
2023-11-11 03:50:08 +0500

    Add project files.

---
+++ BinaryClassificationML/Program.cs
@@ -0,0 +1,11 @@
+﻿//Load sample data
+using BinaryClassificationML;
+
+var sampleData = new MLModel1.ModelInput()
+{
+    Col0 = @"Crust is good.",
+};
+
+//Load model and predict output
+var result = MLModel1.Predict(sampleData);
+Console.WriteLine(result.ToString());
